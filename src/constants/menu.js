c86e669a9aa31afa5083a53cac377b480ea572e9
Fabricio Bracco <Braccof.0601@gmail.com>
2023-08-25 09:36:13 -0300

    Se finalizo el primer tp.

---
+++ src/constants/menu.js
@@ -0,0 +1,26 @@
+export const drawerMenu = [
+    {
+        title: "Home",
+        path: "/",
+    },
+    {
+        title: "Pages",
+        children: [
+            {
+                title: "Todo",
+                path: "/todo"
+            },
+            {
+                title: "FetchList",
+                path: "/fetchList"
+            }
+        ]
+    }
+]
+
+export const popMenu = [
+    {
+        title: "Home",
+        path: "/",
+    },
+]
