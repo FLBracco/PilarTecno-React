319591d437a36586ceeb464ceb6851af83c4d29f
Fabricio Bracco <Braccof.0601@gmail.com>
2023-08-25 23:26:31 -0300

    Se fuciona la rama main con la rama tp2

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -11,57 +11,59 @@ const Dashboard = () => {
     const pendingTask = todoList.filter(task => !task.completed);
 
     return (
-        <Grid container spacing={3}>
+        <>
+            <Grid container spacing={3}>
+                <Grid item xs={6}>
+                    <Link to="/todo">
+                        <Paper sx={{ p: 2 }}>
+                            <Box>
+                                <h3>Pagina To Do</h3>
+                                <Button variant='outlined' color='secondary'>Ir a To Do</Button>
+                            </Box>
+                        </Paper>
+                    </Link>
+                </Grid>
+                <Grid item xs={6}>
+                    <Link to="/fetchlist">
+                        <Paper sx={{ p: 2 }}>
+                            <Box>
+                                <h3>Pagina FetchList</h3>
+                                <Button variant='outlined' color='secondary'>Ir a FetchList</Button>
+                            </Box>
+                        </Paper>
+                    </Link>
+                </Grid>
+            </Grid>
+            <Grid container spacing={3}>
             <Grid item xs={6}>
-                <Link to="/todo">
-                    <Paper sx={{ p: 2 }}>
+                    <Paper sx={{ p: 2 }} variant='outlined'>
                         <Box>
-                            <h3>Pagina To Do</h3>
-                            <Button variant='outlined' color='secondary'>Ir a To Do</Button>
+                            <Card variant="outlined">
+                                <Typography variant="h4">Tareas Completadas</Typography>
+                                <Typography variant="h5">
+                                    Tareas Completadas: {completedTask.length}
+                                </Typography>
+                                <Typography variant='h5'>
+                                    Tareas Totales: {todoList.length}
+                                </Typography>
+                            </Card>
                         </Box>
                     </Paper>
-                </Link>
             </Grid>
             <Grid item xs={6}>
-                <Link to="/fetchlist">
-                    <Paper sx={{ p: 2 }}>
-                        <Box>
-                            <h3>Pagina FetchList</h3>
-                            <Button variant='outlined' color='secondary'>Ir a FetchList</Button>
-                        </Box>
+                    <Paper sx={{ p: 2 }} variant="outlined">
+                            <Card variant='outlined'>
+                                <Typography variant="h4">Tareas Pendientes</Typography>
+                                <Typography variant="h5">
+                                    Tareas Pendientes: {pendingTask.length}
+                                </Typography>
+                                <Typography variant="h5">
+                                    Tareas Totales: {todoList.length}
+                                </Typography>
+                            </Card>
                     </Paper>
-                </Link>
             </Grid>
         </Grid>
-        <Grid container spacing={3}>
-        <Grid item xs={6}>
-                <Paper sx={{ p: 2 }} variant='outlined'>
-                    <Box>
-                        <Card variant="outlined">
-                            <Typography variant="h4">Tareas Completadas</Typography>
-                            <Typography variant="h5">
-                                Tareas Completadas: {completedTask.length}
-                            </Typography>
-                            <Typography variant='h5'>
-                                Tareas Totales: {todoList.length}
-                            </Typography>
-                        </Card>
-                    </Box>
-                </Paper>
-        </Grid>
-        <Grid item xs={6}>
-                <Paper sx={{ p: 2 }} variant="outlined">
-                        <Card variant='outlined'>
-                            <Typography variant="h4">Tareas Pendientes</Typography>
-                            <Typography variant="h5">
-                                Tareas Pendientes: {pendingTask.length}
-                            </Typography>
-                            <Typography variant="h5">
-                                Tareas Totales: {todoList.length}
-                            </Typography>
-                        </Card>
-                </Paper>
-        </Grid>
-    </Grid>
+    </>
     );
 };
