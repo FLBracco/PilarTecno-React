33095482ad898652e9187a12486fa5031853c1f8
Fabricio Bracco <Braccof.0601@gmail.com>
2023-08-26 00:04:03 -0300

    Se arregla el ultimo error en el menu drawer.

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -12,7 +12,7 @@ export const drawerMenu = [
             },
             {
                 title: "FetchList",
-                path: "/fetch-list"
+                path: "/fetchList"
             }
         ]
     }
